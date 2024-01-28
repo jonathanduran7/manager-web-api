783745cc8305f3b3252da0e94308823bddfe798b
jonathanduran7 <joniduran624@gmail.com>
2024-01-27 23:01:10 -0300

    entity account

---
+++ src/main/java/com/jduran/apimanager/entities/Account.java
@@ -0,0 +1,25 @@
+package com.jduran.apimanager.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "accounts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "currency_id", nullable = false)
+    private Currency currency;
+}
