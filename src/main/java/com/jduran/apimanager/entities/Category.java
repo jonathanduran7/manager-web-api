9613af2e6c568f26aed6bce576a695006681720e
jonathanduran7 <joniduran624@gmail.com>
2024-01-28 10:23:09 -0300

    entity category

---
+++ src/main/java/com/jduran/apimanager/entities/Category.java
@@ -0,0 +1,20 @@
+package com.jduran.apimanager.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
