bfc219f8d5d7f524b29153137993f8e3cf2a483a
jonathanduran7 <joniduran624@gmail.com>
2024-01-30 21:10:55 -0300

    implement currency service in controller

--- src/main/java/com/jduran/apimanager/controllers/CurrencyController.java
+++ src/main/java/com/jduran/apimanager/controllers/CurrencyController.java
@@ -1,16 +1,27 @@
 package com.jduran.apimanager.controllers;
 
+import com.jduran.apimanager.entities.Currency;
+import com.jduran.apimanager.services.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/currency")
 public class CurrencyController {
 
+    @Autowired
+    CurrencyService currencyService;
+
     @GetMapping("/")
-    public String getCurrencies() {
-        return "ALL CURRENCIES";
+    @ResponseBody
+    public List<Currency> getCurrencies() {
+        return currencyService.findAll();
     }
 
     @GetMapping("/{id}")
